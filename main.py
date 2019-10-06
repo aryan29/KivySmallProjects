0755df5b17b4ba30e9651a4d77331aeb10d627b6
Aryan Khandelwal <nk28agra@gmail.com>
2019-10-07 03:20:39 +0530

    Finall internet problem solved

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from kivy.animation import Animation
 from kivy.uix.camera import Camera
 from kivy.network.urlrequest import UrlRequest
 import os
+import certifi as cfi
 class MyApp(App):
     def build(self):
         return ScreenManagement(transition=SlideTransition())
@@ -26,27 +27,37 @@ class Screen2(Screen):
     def on_enter(self, *args):
         user=self.manager.ids.sc1.display.text
         print(user)
-        x=UrlRequest(f"https://codeforces.com/api/user.info?handles={user}")
+        def bug_posted(x, result):
+            print('An error is detected',result['comment'])
+            label=self.ids['labeltext']
+            label.text=result['comment']
+        x=UrlRequest(f"https://codeforces.com/api/user.info?handles={user}",on_failure=bug_posted,ca_file=cfi.where(), verify=True)
         x.wait()
-        rating=x.result['result'][0]['rating']
-        print(rating)
-        label=self.ids['labeltext']
-        label.text=f"User {user} rating is {rating}"
+        try:
+            rating=x.result['result'][0]['rating']
+            print(rating)
+            label=self.ids['labeltext']
+            label.text=f"User {user} rating is {rating}"
+        except:
+            print("Already given error report")
 class Screen3(Screen):
     def on_touch_down(self, touch):
         print(touch)
         with self.canvas:
-            Color(1, 0,2)
+            Color(1,1,0)
             touch.ud["line"] = Line(points=(touch.x, touch.y),width=10)
-class Screen4(Screen):
-    pass
-
     def on_touch_move(self, touch):
         print(touch)
         touch.ud["line"].points += (touch.x, touch.y)
-
     def on_touch_up(self, touch):
         print("RELEASED!",touch)
+        #Screen.name="hello"
+class Screen4(Screen):
+    pass
+class Screen5(Screen):
+    def function(self):
+        self.manager.ids.sc3.canvas.add(Color(1., 1., 0))
+        print("hello")
 
 
 Builder.load_file('a.kv')
